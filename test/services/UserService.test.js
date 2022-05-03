143e81c1ba498a86bb284e8ce6d9d310ffdd2d30
Jesus Larios <sau30jas@gmail.com>
2022-05-03 16:50:25 -0500

    Agregando pruebas usando TDD

---
+++ test/services/UserService.test.js
@@ -0,0 +1,10 @@
+const UserService = require('./../../app/services/UserService')
+describe("Test for UserService", () => {
+    test("1. Creaste a new user using the UserService", () => {
+        const user = UserService.create(1, "carlogilmar", "Carlo")
+        expect(user.username).toBe("carlogilmar")
+        expect(user.name).toBe("Carlo")
+        expect(user.id).toBe(1)
+        expect(user.bio).not.toBeUndefined()
+    })
+})
